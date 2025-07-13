18815446789a451b3c61d1e1ec167edb80eedf70
acorbaq <anbaco185@gmail.com>
2025-07-13 20:11:01 +0200

    Rediseño base del menu home para incluir un temporizador clásico estilo Pomodoro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonText, IonButton, IonGrid, IonRow, IonCol, } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+const Home = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Pomodoro App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="ion-padding" color="light">
+        <div style={{ textAlign: 'center', marginTop: '10vh' }}>
+          <IonText color="dark" style={{ fontSize: '64px' }}>
+            <p>25:00</p>
+          </IonText>
+          <IonGrid>
+            <IonRow class="ion-justify-content-center ion-padding-top">
+              <IonCol size="auto">
+                <IonButton color="primary">Iniciar</IonButton>
+              </IonCol>
+              <IonCol size="auto">
+                <IonButton color="medium">Pausar</IonButton>
+              </IonCol>
+              <IonCol size="auto">
+                <IonButton color="danger">Reiniciar</IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
       </IonContent>
     </IonPage>
   );
